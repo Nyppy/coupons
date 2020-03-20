b279f475cded0fcb6582cfccc7de1255cfaf8b88
Juto <Jutonish123@mail.ru>
2020-03-20 15:26:34 +0300

    foot

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-
+    <div class="footer">
+        <nav>
+            <a href="">О нас</a>
+            <a href="">Разместить купон</a>
+            <a href="">Партнерская программа</a>
+            <a href="">Политика конфиденциальности</a>
+            <a href="">Условия использования</a>
+        </nav>
     </div>
 </template>
 
@@ -10,5 +16,22 @@
 </script>
 
 <style scoped>
+    .footer {
+        height: 90px;
+        text-align: center;
+    }
 
+    .footer a {
+        margin-left: 70px;
+        opacity: -1;
+        color: #2e3d4c;
+        font-family: "Gotham Pro";
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 30px;
+    }
+
+    .footer nav a:hover {
+        color: #44cd48;
+    }
 </style>
