ef0b9690f2d5312f914e3084a545f41eca049514
VasiL1X <vlasenko199722@gmail.com>
2020-04-20 02:37:05 +0300

    add: some styles to place coupon page

--- src/views/AppPostCouponPage.vue
+++ src/views/AppPostCouponPage.vue
@@ -42,7 +42,7 @@
                                         </div>
                                         <div class="card-form__price">
                                             <input 
-                                                class="card-form_price-input"
+                                                class="card-form__price-input"
                                                 placeholder="0 $ (FREE)"
                                                 type="text">
                                         </div>
@@ -87,7 +87,9 @@
                     </div>
                     <div class="coupon-preview">
                         <div class="coupon-preview_inner">
-                            <img class="coupon-preview__image" :src="imageSrc" alt="">
+                            <div class="coupon-preview__image-wrapper">
+                                <img class="coupon-preview__image" :src="imageSrc" alt="">
+                            </div>
                             <div class="coupon-preview__price-wrapper">
                                 <div class="coupon-preview__sale">Скидка {{couponSale}}</div>
                                 <!-- Тут должна быть суть скидки -->
@@ -165,26 +167,173 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.Image-input__image-wrapper {
-    flex-basis: 80%;
-    height: 150px;
-    flex: 2.5;
-    border-radius: 1px;
-    margin-right: 10px;
-    overflow-y: hidden;
-    border-radius: 1px;
-    background: #eee;
-    justify-content: center;
-    align-items: center;
+.place-form {
+    width: 100%;
+    min-height: 100%;
     display: flex;
+    flex-direction: column;
+}
+.header__wrapper {
+    flex: 0 0 auto;
+}
+.place-form__wrapper {
+    flex: 1 0 auto;
+}
+.footer__wrapper {
+    flex: 0 0 auto;
+}
+.container {
+    max-width: 1230px;
+    padding: 0 15px;
+    margin: 0 auto;
+}
+.place-form__inner {
+    display: flex;
+    min-height: 540px;
+}
+.coupon-form {
+    flex-basis: 50%;
+    display: flex;
+    text-align: left;
+    padding-right: 20px;
+    border-right: 1px solid #dadada;
+    &__form {
+        max-width: 475px;
+        width: 100%;
+    }
+    &__title {
+        font-size: 24px;
+        line-height: 30px;
+        font-weight: 700;
+        color: #2e3d4c;
+    }
+}
+/*input image*/
+.card-form__name, 
+.card-form__address,
+.card-form__descr,
+.card-form__sale,
+.card-form__price {
+    width: 100%;
+    border-bottom: 1px solid #2e3d4c;
+    position: relative;
+    &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+    }    
+}
+.card-form__name,
+.card-form__descr {
+    &::before {
+        background-image: url(../assets/img/payments/pencil.svg);
+        width: 33px;
+        height: 33px;
+    }
+}
+.card-form__address {
+    &::before {
+        background-image: url(../assets/img/payments/gps.svg);
+        width: 37px;
+        height: 33px;
+        background-size: contain;
+    }
+}
+.card-form__sale {
+    &::before {
+        background-image: url(../assets/img/payments/sales.svg);
+        width: 33px;
+        height: 33px;
+        background-size: contain;
+    }
+}
+.card-form__price {
+    &::before {
+        background-image: url(../assets/img/payments/money.svg);
+        width: 29px;
+        height: 35px;
+        background-size: contain;
+    }
+}
+.card-form__sale,
+.card-form__price {
+    &::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+}
+/*red star*/
+.card-form__name, 
+.card-form__address,
+.card-form__descr {
+    &::after {
+        content: '*';
+        font-size: 18px;
+        font-weight: 700;
+        color: red;
+        position: absolute;
+        top: 17px;
+    }
+    &:focus-within {
+        &::after {
+            display: none;
+        }
+    }
+}
+
+.card-form__name-input, 
+.card-form__address-input,
+.card-form__descr-input,
+.card-form__sale-input,
+.card-form__price-input {
+    width: 100%;
+    border: 0;
+    padding: 25px 10px 15px 55px;
+    font-size: 18px;
+    font-weight: 400;
+    outline: none;
+    &::placeholder {
+        font-size: 16px;
+        font-weight: 400;
+        font-family: 'Montserrat', sans-serif;
+        color: #696969;
+        display: none;
+    }
+    &:focus {
+        &::placeholder {
+            font-size: 0;
+        }
+    }
 }
 
 
-.Image-input__image {
-    max-width: 100%;
-    border-radius: 1px;
+.coupon-preview {
+    flex-basis: 50%;
 }
+// .Image-input__image-wrapper {
+//     flex-basis: 80%;
+//     height: 150px;
+//     flex: 2.5;
+//     border-radius: 1px;
+//     margin-right: 10px;
+//     overflow-y: hidden;
+//     border-radius: 1px;
+//     background: #eee;
+//     justify-content: center;
+//     align-items: center;
+//     display: flex;
+// }
+
+
+// .Image-input__image {
+//     max-width: 100%;
+//     border-radius: 1px;
+// }
 
 .card-form__image {
     overflow: hidden;
@@ -208,13 +357,28 @@ export default {
 .card-form__image-input {
     cursor: inherit;
     display: block;
-    font-size: 999px;
     min-height: 100%;
     opacity: 0;
-    position: absolute;
-    right: 0;
     text-align: right;
-    top: 0;
     cursor: pointer;
+}
+.coupon-preview {
+    &__image {
+        
+        
+        width: 100%;
+        max-height: 100%;
+        border-radius: 7px;
+        
+        
+        &-wrapper {
+            background-color: #707070;
+            max-width: 370px;
+            height: 160px;
+            margin: 0 auto;
+            border-radius: 7px;
+            
+        }
+    }
 }
 </style>
